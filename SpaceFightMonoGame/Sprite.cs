7f62bc71f2cde6699146cf0374a401ddfe5a7330
Kevin Isom <kevin.isom@gmail.com>
2013-03-20 16:54:43 +1300

    created project added graphics

---
+++ SpaceFightMonoGame/Sprite.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceFightMonoGame
+{
+	public class Sprite
+	{
+		protected readonly Texture2D Texture;
+		protected readonly Vector2 Location;
+
+		public Sprite(Texture2D texture, Vector2 location)
+		{
+			Texture = texture;
+			Location = location;
+		} 
+
+		public void Draw(SpriteBatch batch)
+		{
+			
+		}
+	}
+}
